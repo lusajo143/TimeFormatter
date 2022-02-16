b8731057ef4d4b8c946fbbbac12b2a62d548f3e0
lusajo143 <techdealers143@gmail.com>
2022-02-16 05:13:23 +0300

    version 1.0.1

---
+++ app/src/main/java/tz/co/techdealers/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package tz.co.techdealers.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+
+import org.json.JSONException;
+
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        }
+}
